b4cf1abddbee8e30503d6455e18bb3162873d629
Payam Qorbanpour <payamqorbanpour@gmail.com>
2022-04-11 09:07:24 +0430

    Add test3

---
+++ test3-longest substring without repeating characters.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+	s := "abcabcbb"
+
+	fmt.Println(lengthOfLongestSubstring(s))
+}
+
+func lengthOfLongestSubstring(s string) int {
+	m := make(map[byte]int)
+	res := 0
+
+	for l, r := 0, 0; r < len(s); r++ {
+		if index, ok := m[s[r]]; ok {
+			l = max(l, index)
+		}
+		res = max(res, r-l+1)
+		m[s[r]] = r + 1
+	}
+	return res
+}
+
+func max(n, m int) int {
+	if n > m {
+		return n
+	}
+	return m
+}
