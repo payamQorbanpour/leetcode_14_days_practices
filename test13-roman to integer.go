a743bf676e39463398d883e6096824cdc0b83f46
Payam Qorbanpour <payamqorbanpour@gmail.com>
2022-05-08 07:54:42 +0430

    Submit 13

--- test13-roman to integer.go
+++ test13-roman to integer.go
@@ -26,15 +26,13 @@ func romanToInt(s string) int {
 		'M': 1000,
 	}
 
-	output := 0
-	for i, _ := range s {
-		integer, _ := romanMap[s[i]]
-		if i != 0 && (s[i] == 'V' || s[i] == 'X' || s[i] == 'L' || s[i] == 'C' || s[i] == 'D' || s[i] == 'M') {
-			if s[i-1] == 'I' || s[i-1] == 'X' || s[i-1] == 'C' {
-				integer = romanMap[s[i]] - (romanMap[s[i-1]] + romanMap[s[i-1]])
-			}
+	output := romanMap[s[0]]
+	for i := 1; i < len(s); i++ {
+		if romanMap[s[i-1]] < romanMap[s[i]] {
+			output += romanMap[s[i]] - (2 * romanMap[s[i-1]])
+		} else {
+			output += romanMap[s[i]]
 		}
-		output += integer
 	}
 	return output
 }
